3aac7e06b6c6f5156350fe833f3d36a90ba3a144
shantanuparabumd <sparab@umd.edu>
2024-02-09 15:07:10 -0500

    Task 0.1: Enivironemnt Steup

---
+++ scripts/key_board.py
@@ -0,0 +1,97 @@
+#!/usr/bin/env python3
+
+import rclpy
+from rclpy.node import Node
+from sensor_msgs.msg import JointState
+import sys
+import select
+import tty
+import termios
+from pynput import keyboard
+from geometry_msgs.msg import Twist
+
+# Define key codes
+LIN_VEL_STEP_SIZE = 10
+ANG_VEL_STEP_SIZE = 0.1
+
+class KeyboardControlNode(Node):
+
+    def __init__(self):
+        super().__init__('keyboard_control_node')
+
+        self.vel_pub = self.create_publisher(Twist, '/turtlebot3/cmd_vel', 10)
+
+        self.settings = termios.tcgetattr(sys.stdin)
+
+    def getKey(self):
+        tty.setraw(sys.stdin.fileno())
+        rlist, _, _ = select.select([sys.stdin], [], [], 0.1)
+        if rlist:
+            key = sys.stdin.read(1)
+        else:
+            key = ''
+
+        termios.tcsetattr(sys.stdin, termios.TCSADRAIN, self.settings)
+        return key
+
+    def run_keyboard_control(self):
+        self.msg = """
+        Control Your Car!
+        ---------------------------
+        Moving around:
+             w     
+        a    s    d
+
+        q : force stop
+       
+        Esc to quit
+        """
+
+        self.get_logger().info(self.msg)
+        # Create a Twist message
+        twist_msg = Twist()
+        
+        linear_vel=0.0
+        steer_angle=0.0
+        
+
+        while True:
+            key = self.getKey()
+            if key is not None:
+                if key == '\x1b':  # Escape key
+                    break
+                elif key == 'q':  # Quit
+                    linear_vel=0.0
+                    steer_angle=0.0
+                elif key == 'w':  # Forward
+                    linear_vel += LIN_VEL_STEP_SIZE
+                elif key == 's':  # Reverse
+                    linear_vel -= LIN_VEL_STEP_SIZE
+                elif key == 'd':  # Right
+                    steer_angle -= ANG_VEL_STEP_SIZE
+                elif key == 'a':  # Left
+                    steer_angle += ANG_VEL_STEP_SIZE
+
+
+                if steer_angle>2.0:
+                        steer_angle=2.0
+                if steer_angle<-2.0:
+                    steer_angle=-2.0
+
+                print("Steer Angle",steer_angle)
+                print("Linear Velocity",linear_vel)
+                # Publish the twist message
+                twist_msg.linear.x = linear_vel
+                twist_msg.angular.z = steer_angle
+
+                self.vel_pub.publish(twist_msg)
+
+def main(args=None):
+    rclpy.init(args=args)
+    node = KeyboardControlNode()
+    node.run_keyboard_control()
+    node.destroy_node()
+    rclpy.shutdown()
+
+if __name__ == '__main__':
+    main()
